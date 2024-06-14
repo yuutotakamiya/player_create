d62f1f9be7ad602e64ca595291ec98b102200082
S-JOBI23-083\student <231065@st.yoshida-g.ac.jp>
2024-06-14 09:31:32 +0900

    VisualStudio2022でプロジェクトを作成 #1

---
+++ ConsoleAppGE2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGE2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
